36c914230b8cff66e1152e813101b1a48e737ba5
ifgladlee <ifgladlee@gmail.com>
2015-04-03 09:03:40 +0800

    project import

---
+++ config_external.h
@@ -0,0 +1,16 @@
+// config.h automatically created 2015/04/02 ÷‹Àƒ 16:41:18.06 
+// ..\utilities\configure: config=config_external.h 
+// ..\utilities\configure: search=C:\ 
+// ..\utilities\configure: source=.. 
+// : MYSQL target include\mysql.h not found  
+// ..\utilities\configure: MATLAB target extern\lib\platform\libmx.lib not found  
+// ..\utilities\configure: MATLAB target extern\lib\platform\libmx.lib not found  
+// ..\utilities\configure: MATLAB target extern\lib\platform\libmx.lib not found  
+// ..\utilities\configure: MATLAB target extern\lib\platform\libmx.lib not found  
+// ..\utilities\configure: MATLAB target extern\lib\platform\libmx.lib not found  
+// ..\utilities\configure: MATLAB target extern\lib\platform\libmx.lib not found  
+// ..\utilities\configure: MATLAB target extern\lib\platform\libmx.lib not found  
+// ..\utilities\configure: MATLAB target extern\lib\platform\libmx.lib not found  
+// ..\utilities\configure: MATLAB target extern\lib\platform\libmx.lib not found  
+// ..\utilities\configure: MATLAB target extern\lib\platform\libmx.lib not found  
+// ..\utilities\configure: PowerWorld SimAuto target RegSimAuto.exe not found  
